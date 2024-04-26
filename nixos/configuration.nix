c2e28fb44848bd9622658198ac246b4e9f202f0e
Kevin Houtz <khoutz182@pm.me>
2024-04-26 00:03:10 -0500

    trying out leftwm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -44,15 +44,16 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
   services.xserver = {
+    # Enable the X11 windowing system.
+    enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    displayManager.lightdm.enable = true;
+    desktopManager.gnome.enable = true;
+    windowManager.leftwm.enable = true;
+    
+    # Configure keymap in X11
     layout = "us";
     xkbVariant = "";
   };
